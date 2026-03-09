43451d02b1f03d86b28df96f7c2729f86b77c717
Subhasish Lakshman <59129320+subhasishlak123@users.noreply.github.com>
2026-03-09 16:25:14 +0530

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,6 @@
+// ROOT build.gradle.kts
 plugins {
     id("com.android.application") version "8.7.0" apply false
     id("org.jetbrains.kotlin.android") version "2.0.20" apply false
     id("org.jetbrains.kotlin.plugin.compose") version "2.0.20" apply false
-}
-
-// This forces the entire build environment to use Java 17
-kotlin {
-    jvmToolchain(17)
 }
