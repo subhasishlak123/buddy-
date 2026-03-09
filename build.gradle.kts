539ab7c6995c75907320aa6b362829ebe940b6ea
Subhasish Lakshman <59129320+subhasishlak123@users.noreply.github.com>
2026-03-09 16:19:14 +0530

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,10 @@
-// ROOT build.gradle.kts
 plugins {
-    // Upgrading to 8.7.0+ for compatibility with Gradle 9
     id("com.android.application") version "8.7.0" apply false
     id("org.jetbrains.kotlin.android") version "2.0.20" apply false
-    // Compose Compiler is now part of the Kotlin plugin (2.0+)
     id("org.jetbrains.kotlin.plugin.compose") version "2.0.20" apply false
+}
+
+// This forces the entire build environment to use Java 17
+kotlin {
+    jvmToolchain(17)
 }
