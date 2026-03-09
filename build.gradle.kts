2adbfae18a3d609f6778e80695e24233599eae0e
Subhasish Lakshman <59129320+subhasishlak123@users.noreply.github.com>
2026-03-09 15:48:12 +0530

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,8 @@
 // ROOT build.gradle.kts
 plugins {
-    // We define the versions HERE so the app folder doesn't have to
-    id("com.android.application") version "8.2.0" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.0" apply false
+    // Upgrading to 8.7.0+ for compatibility with Gradle 9
+    id("com.android.application") version "8.7.0" apply false
+    id("org.jetbrains.kotlin.android") version "2.0.20" apply false
+    // Compose Compiler is now part of the Kotlin plugin (2.0+)
+    id("org.jetbrains.kotlin.plugin.compose") version "2.0.20" apply false
 }
