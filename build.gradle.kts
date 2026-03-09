085fda271d7496bf9f2cd656817b8aa726ce5fdc
Subhasish Lakshman <59129320+subhasishlak123@users.noreply.github.com>
2026-03-09 15:42:48 +0530

    Create build.gradle.kts

---
+++ build.gradle.kts
@@ -0,0 +1,6 @@
+// ROOT build.gradle.kts
+plugins {
+    // We define the versions HERE so the app folder doesn't have to
+    id("com.android.application") version "8.2.0" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.0" apply false
+}
